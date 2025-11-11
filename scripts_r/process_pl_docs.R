b871f3a04a6ba403005d59b6144f0a497c8fc29a
scipima <marco.scipioni05@gmail.com>
2025-11-11 17:59:32 +0100

    Extract unnest functions

--- scripts_r/process_pl_docs.R
+++ scripts_r/process_pl_docs.R
@@ -25,6 +25,11 @@ rm(packages)
 
 
 #------------------------------------------------------------------------------#
+## Functions -------------------------------------------------------------------
+source(file = here::here("scripts_r", "unnest_functions.R"))
+
+
+#------------------------------------------------------------------------------#
 ## Data ------------------------------------------------------------------------
 # Test (If there are no new files, just read in what you have in the bank)
 if ( !exists(x = "pl_docs_list") ) {
@@ -34,120 +39,6 @@ if ( !exists(x = "pl_docs_list") ) {
 if ( !exists(x = "pl_docs") ) {
   pl_docs <- data.table::fread(
     file = here::here("data_out", "docs_pl", "pl_docs.csv"))
-}
-
-
-#------------------------------------------------------------------------------#
-## Functions -------------------------------------------------------------------
-
-# General function to unnest nested data structures as data.frame -------------#
-unnest_nested_df <- function(
-    data_list = pl_docs_list, # list
-    group_col = "id", # string, no more than 1 col
-    unnest_col = "workHadParticipation" # string
-){
-  list_unnest_tmp <- lapply(
-    X = data_list, FUN = function(doc) {
-      # Check whether the col to be unnested is actually present
-      if ( unnest_col %in% names(doc)
-           && !is.null(doc[[unnest_col]])
-           && length(doc[[unnest_col]]) > 0 ) {
-
-        # Check whether data is DT or DF
-        if ( is.data.table(doc) ) {
-          cols_tokeep = c(group_col, unnest_col)
-          doc_subset <- doc[, ..cols_tokeep, with = FALSE]
-        } else if ( !is.data.table(doc) && is.data.frame(doc) ) {
-          doc_subset <- data.table::as.data.table(
-            doc[, c(group_col, unnest_col)]
-          )
-        }
-
-        # Unnest data
-        unnest_dt = data.table::rbindlist(
-          l = setNames(
-            object = doc_subset[[unnest_col]],
-            nm = doc_subset[[group_col]]), # rename on the fly
-          use.names = TRUE, fill = TRUE, idcol = "group_col_tmp"
-        )
-
-        # Return unnested DT
-        return(unnest_dt)
-      }
-    }
-  )
-  # Append all temporary unnested DT ------------------------------------------#
-  result = data.table::rbindlist(list_unnest_tmp, use.names = TRUE, fill = TRUE)
-
-  # Check if `group_col` is present in unnested DF
-  if ( group_col %in% names(result) ) {
-    cat("\nOne of the unnested cols within the DT has the same name of the grouping col. Proceed to rename it...\n")
-    data.table::setnames(
-      x = result,
-      old = c("group_col_tmp", group_col),
-      new = c(group_col, paste0(unnest_col, "_id")))
-  }
-  return(result)
-}
-
-# General function to unnest nested data structures as character list ---------#
-unnest_nested_list <- function(
-    data_list = pl_docs_list, # list
-    group_cols = "id", # string, can be a vector of cols
-    unnest_col = "is_derivative_of" # string
-) {
-
-  list_unnest_tmp <- lapply(
-    X = data_list, FUN = function(doc) {
-      # Check whether the col to be unnested is actually present
-      if ( unnest_col %in% names(doc)
-           && !is.null(doc[[unnest_col]])
-           && length(doc[[unnest_col]]) > 0 ) {
-
-        # Check whether data is DT or DF
-        if ( is.data.table(doc) ) {
-          cols_tokeep = c(group_cols, unnest_col)
-          doc_subset <- doc[, ..cols_tokeep, with = FALSE]
-        } else if ( !is.data.table(doc) && is.data.frame(doc) ) {
-          doc_subset <- data.table::as.data.table(
-            doc[, c(group_cols, unnest_col)]
-          )
-        }
-
-        # Check if ALL cells are NULL - if so, return NULL
-        if ( all(sapply(doc_subset[[unnest_col]], is.null)) ) {
-          return(NULL)
-        } else if ( any(sapply(doc_subset[[unnest_col]], is.null)) ) {
-          # Check if ANY cells are NULL - if so, kick them out
-          doc_subset = doc_subset[
-            !sapply( doc_subset[[unnest_col]], is.null ),
-          ]
-        }
-
-        # Determine cols to group by (all cols *except* unnest col).
-        # This ensures all non-unnested data is carried over and duplicated correctly.
-        grouping_cols <- setdiff(names(doc_subset), unnest_col)
-
-        # Perform the unnesting using Standard Evaluation (SE)
-        # - `by = grouping_cols`: Passes the vector of column names for grouping.
-        # - `list(unnest_col_tmp = unlist(get(unnest_col)))`:
-        #    - `get(unnest_col)` accesses the list column by its string name.
-        #    - `unlist()` flattens the list.
-        #    - The result is temporarily named `unnest_col_tmp`.
-        unnest_dt <- doc_subset[, list(unnest_col_tmp = unlist(get(unnest_col))),
-                                by = grouping_cols]
-
-        # Rename the temporary col 'unnest_col_tmp' back to `unnest_col`.
-        setnames(unnest_dt, old = "unnest_col_tmp", new = unnest_col)
-
-        return(unnest_dt)
-      }
-    }
-  )
-  # Append all temporary unnested DT ------------------------------------------#
-  result = data.table::rbindlist(list_unnest_tmp, use.names = TRUE, fill = TRUE)
-
-  return(result)
 }
 
 
